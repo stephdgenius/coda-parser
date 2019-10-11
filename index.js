1ded0de13118fdfe516fd7d14504759d0bf05868
Steph DGenius <stephanetse@gmail.com>
2019-10-11 15:19:07 +0100

    :tada: base structure

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/authcallback", (req, res) => {
+	const app_details = {
+		app_name: "Afrologix Coda parser",
+		auth: true
+	};
+	res.json(app_details);
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
